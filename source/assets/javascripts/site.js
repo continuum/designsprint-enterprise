1dc841d879279c597ce6d34a6c7241ec315ef373
Ricardo Alfaro <ricardoalfarog@gmail.com>
2018-06-18 16:33:21 -0400

    Add structure project

---
+++ source/assets/javascripts/site.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+  //Responsive Nav Menu
+  $('.toggle-nav-mobile').click(function(){
+    $('.header-nav').addClass('display');
+  })
+  $('.header-nav-out').click(function(){
+    $('.header-nav').removeClass('display');
+  })
+  
+});
