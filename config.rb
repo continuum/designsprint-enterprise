49243682448b70007999eac84408bdf40ce2268d
Ricardo Alfaro <ricardoalfarog@gmail.com>
2018-06-18 16:33:06 -0400

    Add Gem Files

---
+++ config.rb
@@ -0,0 +1,82 @@
+###
+# Compass
+###
+
+# Change Compass configuration
+# compass_config do |config|
+#   config.output_style = :compact
+# end
+
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+# page "/path/to/file.html", :layout => false
+#
+# With alternative layout
+# page "/path/to/file.html", :layout => :otherlayout
+#
+# A path which all have the same layout
+# with_layout :admin do
+#   page "/admin/*"
+# end
+
+# Proxy pages (https://middlemanapp.com/advanced/dynamic_pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
+#  :which_fake_page => "Rendering a fake page with a local variable" }
+
+#data.casos.keys.each do |caso|
+  #proxy "pages/casos/#{caso}/index.html", "pages/casos/template.html", :locals => { :case_page => caso }
+#end
+
+#ignore "pages/casos/template.html"
+
+###
+# Helpers
+###
+
+# Automatic image dimensions on image_tag helper
+# activate :automatic_image_sizes
+
+#activate :autoprefixer do |prefix|
+#  prefix.browsers = "last 2 versions"
+#end
+
+# Activate Languages
+#activate :i18n, :mount_at_root => :es
+
+# Pretty URLs 
+# activate :directory_indexes
+
+# Live Reloaded
+activate :livereload
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+set :css_dir, 'assets/stylesheets'
+set :js_dir, 'assets/javascripts'
+set :images_dir, 'assets/images'
+#activate :i18n, mount_at_root: :es
+activate :minify_css
+
+#activate :directory_indexes
+
+
+# Build-specific configuration
+# https://middlemanapp.com/advanced/configuration/#environment-specific-settings
+
+configure :build do
+#   activate :minify_css
+#   activate :minify_javascript
+  activate :relative_assets
+end
+
+proxy "_redirects", "netlify-redirects", ignore: true
